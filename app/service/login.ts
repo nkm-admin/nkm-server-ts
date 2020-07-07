725d1aa1d9e2567c8f9371301ceb02732aa99fae
xuanmos <me@xuanmo.xin>
2020-07-08 00:29:53 +0800

    fix: 修复登录时，资源为页面权限不返回问题

--- app/service/login.ts
+++ app/service/login.ts
@@ -77,6 +77,9 @@ export default class Login extends Service {
             menu.push(item)
             menuUrls.push(item.path)
             break
+          case 'system:resource:page':
+            menuUrls.push(item.path)
+            break
           case 'system:resource:btn':
             btnCodes.push(item.code)
             break
