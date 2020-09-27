c27303bd070e04757a99401880fdc8a2979e82aa
D-xuanmo <me@xuanmo.xin>
2020-09-27 08:54:56 +0800

    chore: 应用启动增加log

--- app.ts
+++ app.ts
@@ -12,11 +12,24 @@ class AppBootHook {
     console.log('配置文件即将加载')
   }
 
+  configDidLoad() {
+    console.log('配置文件加载完成')
+  }
+
+  didLoad() {
+    console.log('文件加载完成')
+  }
+
   // 插件启动完毕
   async willReady() {
     for (const [_k, _v] of Object.entries(validator)) {
       this.app.validator.addRule(_k, (...[, value]) => _v(value))
     }
+    console.log('插件启动完毕')
+  }
+
+  serverDidReady() {
+    console.log('应用启动完成')
   }
 }
 
